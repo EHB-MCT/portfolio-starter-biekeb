6f0248428eac33e88c5f950a327bd793bedbcd8d
biekeb <91184065+biekeb@users.noreply.github.com>
2023-11-10 11:16:49 +0100

    Fix: making a env file to store the information

--- .github/images/api/db/knexfile.js
+++ .github/images/api/db/knexfile.js
@@ -1,17 +1,9 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: 'localhost',
-      user: 'your_username',
-      password: 'your_password',
-      database: 'usersApi',
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
-      directory: './migrations',
-    },
-    seeds: {
-      directory: './seeds',
+      directory: './migrations', 
     },
   },
 };
