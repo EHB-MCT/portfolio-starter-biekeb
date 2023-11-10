b6069a08f7b2492e8a2cd111b3e7142ddf10afa9
biekeb <91184065+biekeb@users.noreply.github.com>
2023-11-10 11:34:06 +0100

    fix: changing the route if the application

--- .github/images/api/src/index.js
+++ .github/images/api/src/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 const db = knex(require('../db/knexfile').development);
 
-app.get('/api/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
