726cd295444a510ef939acb80ed658810aeec9c5
biekeb <91184065+biekeb@users.noreply.github.com>
2023-12-08 11:59:42 +0100

    Feat: Adding a age table

--- images/api/src/__tests__/helpers.test.js
+++ images/api/src/__tests__/helpers.test.js
@@ -1,30 +1,41 @@
-const {checkUserName, checkUserEmail, checkUserPassword} = require("../helpers/endpointHelpers.js")
+const {
+  checkUserName,
+  checkUserEmail,
+  checkUserPassword,
+  checkUserAge,
+} = require("../helpers/endpointHelpers.js");
 
-test('checkname ', () => {
-    //test name
-    expect(checkUserName("")).toBe(false);
-    expect(checkUserName(null)).toBe(false);
-    expect(checkUserName(undefined)).toBe(false);
-    expect(checkUserName(123)).toBe(false);
-    expect(checkUserName("a")).toBe(false);
-    expect(checkUserName("aaaaaaaaaaaaaaaaaaaaa")).toBe(false);
-    expect(checkUserName("!@#$%^")).toBe(false);
-    expect(checkUserName("JohnDoe")).toBe(true);
-    expect(checkUserName("user name")).toBe(false);
-    expect(checkUserName("JohnDoe123")).toBe(false); 
+test("checkname ", () => {
+  //test name
+  expect(checkUserName("")).toBe(false);
+  expect(checkUserName(null)).toBe(false);
+  expect(checkUserName(undefined)).toBe(false);
+  expect(checkUserName(123)).toBe(false);
+  expect(checkUserName("a")).toBe(false);
+  expect(checkUserName("aaaaaaaaaaaaaaaaaaaaa")).toBe(false);
+  expect(checkUserName("!@#$%^")).toBe(false);
+  expect(checkUserName("JohnDoe")).toBe(true);
+  expect(checkUserName("user name")).toBe(false);
+  expect(checkUserName("JohnDoe123")).toBe(false);
 
-    // Test Email
-    expect(checkUserEmail("john.doe@example.com")).toBe(true);
-    expect(checkUserEmail("invalid.email")).toBe(false);
-    expect(checkUserEmail("john.doe@example")).toBe(false);
+  // Test Email
+  expect(checkUserEmail("john.doe@example.com")).toBe(true);
+  expect(checkUserEmail("invalid.email")).toBe(false);
+  expect(checkUserEmail("john.doe@example")).toBe(false);
 
-    // Test Password
-    expect(checkUserPassword(null)).toBe(false);
-    expect(checkUserPassword(undefined)).toBe(false);
-    expect(checkUserPassword("12345")).toBe(false); 
-    expect(checkUserPassword("weak")).toBe(false); 
-    expect(checkUserPassword("strongPassword123")).toBe(true);
+  // Test Password
+  expect(checkUserPassword(null)).toBe(false);
+  expect(checkUserPassword(undefined)).toBe(false);
+  expect(checkUserPassword("12345")).toBe(false);
+  expect(checkUserPassword("weak")).toBe(false);
+  expect(checkUserPassword("strongPassword123")).toBe(true);
+});
 
-
-})
-
+//Test age
+test("checkage", () => {
+  expect(checkUserAge(null)).toBe(false);
+  expect(checkUserAge(undefined)).toBe(false);
+  expect(checkUserAge("25")).toBe(false);
+  expect(checkUserAge(-5)).toBe(false);
+  expect(checkUserAge(25)).toBe(true);
+});
---
+++ images/api/src/db/migrations/20231207111755_addColumnAge.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("usersApi", (table) => {
+    table.integer("age");
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("usersApi", (table) => {
+    table.dropColumn("age");
+    table.dropColumn(true, true);
+  });
+};
