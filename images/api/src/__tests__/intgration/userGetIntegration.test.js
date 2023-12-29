078fe79969c243299d81e83da8b510ee48415b89
biekeb <91184065+biekeb@users.noreply.github.com>
2023-12-29 14:30:15 +0100

    Update userGetIntegration.test.js

--- images/api/src/__tests__/intgration/userGetIntegration.test.js
+++ images/api/src/__tests__/intgration/userGetIntegration.test.js
@@ -2,62 +2,38 @@ const request = require("supertest");
 const app = require("../../app.js");
 const knexfile = require("../../db/knexfile.js");
 const db = require("knex")(knexfile.development);
+const { expect } = require("chai");
+const { initUserRoutes } = require("../../routes/users.js");
 
 describe("GET /users", () => {
-  beforeAll(async () => {
-    await db.raw("BEGIN");
+  let app;
+
+  before(() => {
+    app = express();
+    initUserRoutes(app, db);
+    // Set up any necessary environment for testing (e.g., initialize a test database)
   });
 
-  afterAll(async () => {
-    await db.destroy();
+  after(async () => {
+    // Clean up resources after testing (e.g., close the test database connection)
   });
 
-  // Use beforeEach to ensure a clean state before each test
-  beforeEach(async () => {
-    // Insert some dummy data into the database for testing
-    await db("usersApi").insert([
-      {
-        name: "User1",
-        email: "user1@example.com",
-        age: 25,
-        password: "passwoOd1",
-      },
-      {
-        name: "User2",
-        email: "user2@example.com",
-        age: 30,
-        password: "passwoOd2",
-      },
-    ]);
+  it("should return an array of users", async () => {
+    const response = await request(app).get("/users").expect(200);
+
+    // Assuming your response is expected to be an array of users
+    expect(response.body).to.be.an("array");
+    expect(response.body).to.have.length.greaterThan(0);
   });
 
-  // Use afterEach to clean up the database after each test
-  afterEach(async () => {
-    // Rollback any changes made during the test
-    await db.raw("ROLLBACK");
-  });
+  it("should handle errors gracefully", async () => {
+    // Simulate an error in the database
+    // For example, you could change the table name in the query to cause an error
+    const response = await request(app).get("/users").expect(500);
 
-  test("should return all users", async () => {
-    const response = await request(app).get("/users");
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveLength(2); // Assuming two users were inserted
-    expect(response.body[0]).toHaveProperty("name", "User1");
-    expect(response.body[1]).toHaveProperty("name", "User2");
-  });
-
-  test("should handle database retrieval error", async () => {
-    // Mock the db select method to throw an error
-    jest.spyOn(db, "select").mockImplementationOnce(() => {
-      throw new Error("Database error");
-    });
-
-    const response = await request(app).get("/users");
-
-    expect(response.status).toBe(500);
-    expect(response.body).toHaveProperty(
-      "error",
-      "Error retrieving users from the database: Database error"
+    expect(response.body).to.have.property("error");
+    expect(response.body.error).to.match(
+      /Error retrieving users from the database/
     );
   });
 });
