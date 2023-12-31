016d371c30ab872668c54c2a6a4386d7cd4c52a6
biekeb <91184065+biekeb@users.noreply.github.com>
2023-12-31 16:16:15 +0100

    Create 20231230202144_addColumnRole.js

---
+++ images/api/src/db/migrations/20231230202144_addColumnRole.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("usersApi", (table) => {
+    table.string("role");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("usersApi", (table) => {
+    table.dropColumn("role");
+  });
+};
